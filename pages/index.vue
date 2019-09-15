3f761f36af2fd6ae082abb9396f0e278dd24f737
OlguinJv <olguin.j.vito@gmail.com>
2019-09-15 00:10:23 -0300

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+  .container nuxt custom template
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="scss">
+  .container {
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+</style>
